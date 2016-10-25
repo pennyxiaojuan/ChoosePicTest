4639be0042ca41225787b40c7cccb1a8986e61e4
penny_xiaojuan <penny_xiaojuan@outlook.com>
2016-10-26 00:30:37 +0800

    Initial commit and  Game Forced Termination.

---
+++ app/src/main/java/com/example/penny/choosepictest/MainActivity.java
@@ -0,0 +1,159 @@
+package com.example.penny.choosepictest;
+
+import android.annotation.TargetApi;
+import android.content.ContentUris;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    public static final int TAKE_PHOTO= 1;
+    public static final int CROP_PHOTO =2;
+    private Button takePhoto;
+    private ImageView picture;
+    private Uri imageUri;
+    public static final int CHOOSE_PHOTO = 3;
+    private Button chooseFromAlbum;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        takePhoto = (Button) findViewById(R.id.take_photo);//获得Button实例
+        picture = (ImageView) findViewById(R.id.picture);//获得ImageView实例
+        takePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //创建file对象,用于储存拍照后的图片。
+                // 调用方法获得SD卡根目录，把图片命名为output_image.jpg
+                File outputImage = new File(Environment.
+                        getExternalStorageDirectory(),"output_image.jpg");
+                try{
+                    if (outputImage.exists()){
+                        outputImage.delete();
+                    }outputImage.createNewFile();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+                imageUri = Uri.fromFile(outputImage);//调用fromFile方法将file转化为Uri对象
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                intent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
+                startActivityForResult(intent,TAKE_PHOTO);//启动相机程序
+            }
+        });
+        chooseFromAlbum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        "android.intent.action.GET_CONTENT");
+                intent.setType("image/*");
+                startActivityForResult(intent,CHOOSE_PHOTO);//打开相册
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,Intent data){
+        switch (requestCode){
+            case TAKE_PHOTO:
+                if (resultCode == RESULT_OK){
+                    Intent intent = new Intent("com.android.camera.action.CROP");
+                    intent.setDataAndType(imageUri,"image/*");
+                    intent.putExtra("scale",true);
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
+                    startActivityForResult(intent,CROP_PHOTO);//启动剪裁程序
+                }break;
+            case CROP_PHOTO:
+                if (resultCode == RESULT_OK){
+                    try{
+                        Bitmap bitmap = BitmapFactory.decodeStream
+                                (getContentResolver().openInputStream(imageUri));
+                        picture.setImageBitmap(bitmap);//将裁剪后的照片显示出来
+                    }catch (FileNotFoundException e){
+                        e.printStackTrace();
+                    }
+                }
+            case CHOOSE_PHOTO:
+                if (resultCode == RESULT_OK){
+                    //判断手机系统版本号
+                    if (Build.VERSION.SDK_INT>=19){
+                        //4.4以上系统使用这个方法处理图片
+                        handleImageOnKitKat(data);
+                    }else{
+                        //4.4以下系统使用这个方法处理图片
+                        handleImageBeforeKitKat(data);
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+    }
+    @TargetApi(19)
+    private void handleImageOnKitKat(Intent data){
+        String imagePath = null;
+        Uri uri = data.getData();
+        if (DocumentsContract.isDocumentUri(this,uri)){
+            //如果是document类型的Uri,则通过document id来处理
+            String docId = DocumentsContract.getDocumentId(uri);
+            if ("com.android.providers.media.documents".equals(uri.getAuthority())){
+                String id = docId.split(":")[1];//解析出数字格式的id
+                String selection = MediaStore.Images.Media._ID +"="+id;
+                imagePath = getImagePath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,selection);
+            }else if ("com.android.providers.downloads.documents".equals(uri.getAuthority())){
+                Uri contentUri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"),
+                        Long.valueOf(docId));
+                imagePath = getImagePath(contentUri,null);
+            }else if ("content".equalsIgnoreCase(uri.getScheme())){
+                //如果不是  则使用普通方式处理
+                imagePath = getImagePath(uri,null);
+            }
+            displayImage(imagePath);//根据图片路径显示图片
+            }
+    }
+        private void handleImageBeforeKitKat(Intent data){
+        Uri uri = data.getData();
+        String imagePath =getImagePath(uri,null);
+        displayImage(imagePath);
+        }
+    private String getImagePath(Uri uri,String selection){
+        String path = null;
+        //通过Uri和selection来获取真是的图片路径
+        Cursor cursor = getContentResolver().query(
+                uri,null,selection,null,null);
+        if (cursor!= null){
+            if (cursor.moveToFirst()){
+                path = cursor.getString(
+                        cursor.getColumnIndex(MediaStore.Audio.Media.DATA));
+            }
+            cursor.close();
+        }
+        return path;
+    }
+    private void displayImage(String imagePath){
+        if (imagePath!=null){
+            Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
+            picture.setImageBitmap(bitmap);
+        }else{
+            Toast.makeText(this,"failed to get image",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
